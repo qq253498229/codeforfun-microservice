e8b51fd900e61e325ea370f2dc0f37ebc8749c77
大胃王 <253498229@qq.com>
2018-07-08 05:10:02 +0800

    test

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /root/codeforfun/codeforfun-microservice
+cd /root/codeforfun-microservice
 docker-compose pull nginx
 docker-compose rm -s -f nginx
 docker-compose up -d nginx
--- webhook.sh
+++ webhook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /root/codeforfun/codeforfun-microservice
+cd /root/codeforfun-microservice
 docker-compose pull webhooks
 docker-compose rm -s -f webhooks
 docker-compose up -d webhooks
