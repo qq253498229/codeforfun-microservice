3cb1e7556c7e4c9e8885071dab17649a400d380b
大胃王 <253498229@qq.com>
2018-07-08 03:48:05 +0800

    test

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd /root/codeforfun
-docker-compose -f docker-compose-base.yml build jenkins
 docker-compose -f docker-compose-base.yml rm -s -f jenkins
 docker-compose -f docker-compose-base.yml up -d jenkins
