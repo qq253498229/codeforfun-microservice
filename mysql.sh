e451d533fe4f9556545fa3900c114ea0c80c40ad
大胃王 <253498229@qq.com>
2018-07-08 07:54:56 +0800

    test

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /root/codeforfun-microservice
-docker-compose -f docker-compose-base.yml pull mysql
-docker-compose -f docker-compose-base.yml rm -s -f mysql
-docker-compose -f docker-compose-base.yml up -d mysql
+docker-compose pull mysql
+docker-compose rm -s -f mysql
+docker-compose up -d mysql
