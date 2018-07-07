dfbed94171c3941c1b6b98eadb39144e5fd8eb47
大胃王 <253498229@qq.com>
2018-07-08 03:59:23 +0800

    test

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd /root/codeforfun/codeforfun-microservice
 docker-compose -f docker-compose-base.yml pull jenkins
+docker-compose -f docker-compose-base.yml rm -s -f jenkins
 docker-compose -f docker-compose-base.yml up -d jenkins
--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-cd /root/codeforfun
+cd /root/codeforfun/codeforfun-microservice
+docker-compose pull nginx
 docker-compose rm -s -f nginx
 docker-compose up -d nginx
