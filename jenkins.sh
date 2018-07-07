430dd82df53c5be206a12b14d81ed98a2f151ac0
大胃王 <253498229@qq.com>
2018-07-08 03:55:42 +0800

    test

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /root/codeforfun
-docker-compose -f docker-compose-base.yml rm -s -f jenkins
+cd /root/codeforfun/codeforfun-microservice
+docker-compose -f docker-compose-base.yml pull jenkins
 docker-compose -f docker-compose-base.yml up -d jenkins
