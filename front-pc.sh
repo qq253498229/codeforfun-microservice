ba2ba20cdffa2a0eb8791b488d43877a593c00d7
大胃王 <253498229@qq.com>
2018-07-08 21:25:45 +0800

    加入前台

--- front-pc.sh
+++ front-pc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /root/codeforfun-microservice
-docker-compose -f docker-compose-base.yml pull front-pc
-docker-compose -f docker-compose-base.yml rm -s -f front-pc
-docker-compose -f docker-compose-base.yml up -d front-pc
+docker-compose pull front-pc
+docker-compose rm -s -f front-pc
+docker-compose up -d front-pc
