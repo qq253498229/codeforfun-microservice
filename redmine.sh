1d79c158d06dd9c8e42a156933641d91299904b8
大胃王 <253498229@qq.com>
2018-07-10 14:41:47 +0800

    Revert "add redmine"
    This reverts commit 3544275

--- redmine.sh
+++ redmine.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /root/codeforfun-microservice
-docker-compose -f docker-compose-base.yml rm -s -f redmine
-docker-compose -f docker-compose-base.yml up -d redmine
+docker-compose rm -s -f redmine
+docker-compose up -d redmine
