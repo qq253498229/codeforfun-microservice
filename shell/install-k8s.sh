8bbb0dbfe384470ab36c26d32533e352205edf48
大胃王 <253498229@qq.com>
2018-09-11 15:08:35 +0800

    update

--- shell/install-k8s.sh
+++ shell/install-k8s.sh
@@ -16,7 +16,8 @@ apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
 #defined images
-export images=(kube-proxy-amd64:v1.11.1 kube-controller-manager-amd64:v1.11.1 kube-scheduler-amd64:v1.11.1 kube-apiserver-amd64:v1.11.1 coredns:1.1.3 etcd-amd64:3.2.18 pause:3.1)
+images=(kube-proxy-amd64:v1.11.1 kube-controller-manager-amd64:v1.11.1 kube-scheduler-amd64:v1.11.1 kube-apiserver-amd64:v1.11.1 coredns:1.1.3 etcd-amd64:3.2.18 pause:3.1)
+
 #pull images about google for k8s
 for image in ${images[@]}; do
   docker pull registry.cn-qingdao.aliyuncs.com/wangdali/$image
